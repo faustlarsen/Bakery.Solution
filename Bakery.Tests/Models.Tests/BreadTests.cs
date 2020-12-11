2efccd53cbda3573d891d5cb1723d53b29d0bd69
Constantine <faustlarsen@gmail.com>
2020-12-11 11:40:32 -0700

    fail test to accept integer

--- Bakery.Tests/Models.Tests/BreadTests.cs
+++ Bakery.Tests/Models.Tests/BreadTests.cs
@@ -7,10 +7,17 @@ namespace Bakery.Tests
   public class TestBread
   {
     [TestMethod]
-    public void BreadCOnstructor_InstanceOfBread_True()
+    public void BreadConstructor_InstanceOfBread_True()
     {
     Bread purchase = new Bread(2);
     Assert.AreEqual(typeof(Bread), purchase.GetType());
+    }
+
+    [TestMethod]
+    public void BreadConstructor_ExceptsAmount_Int()
+    {
+      Bread purchase = new Bread(2);
+      Assert.AreEqual(true, purchase.AmountOfBread(2));
     }
   }
 } 
