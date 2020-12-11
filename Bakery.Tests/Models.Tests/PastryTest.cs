7d8c722666dfcc9b9254afd16272f1c689344ddc
Constantine <faustlarsen@gmail.com>
2020-12-11 12:05:35 -0700

    test if Bread class uploded on github

---
+++ Bakery.Tests/Models.Tests/PastryTest.cs
@@ -0,0 +1,16 @@
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using Bakery.Models;
+
+// namespace Bakery.Tests
+// {
+//   [TestClass]
+//   public class TestPastry
+//   {
+//     [TestMethod]
+//     public void PastryConstructor_InstanceOfPastry_True()
+//     {
+//     Pastry purchase = new Pastry(2);
+//     Assert.AreEqual(typeof(Pastry), purchase.GetType());
+//     }
+//   }
+// }
